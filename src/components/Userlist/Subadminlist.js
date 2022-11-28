22b19aa6a0a85cca41860670de125b0766058c5c
Hitanshu Shah <hitanshushah5@gmail.com>
2022-11-29 01:15:53 +0530

    feat:Subadmin assign and subadmin model list

---
+++ src/components/Userlist/Subadminlist.js
@@ -0,0 +1,79 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Alert, ModalBody } from "react-bootstrap";
+
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import useToken from "../App/useToken";
+
+export default function Subadminlist({ open, setOpen, Ids }) {
+  const { token } = useToken();
+  const [admins, setAdmins] = useState([]);
+  const [selectedsubadmin, setSelectedsubadmin] = useState("");
+  console.log(Ids);
+  const assigntosubadmin = () => {
+    axios
+      .post("http://localhost:3000/application/assign", {
+        token,
+        ProcessorId: selectedsubadmin,
+        Ids,
+      })
+      .then((res) => {
+        setOpen(false);
+        console.log(res.data);
+        alert("Assigned Succesfully!");
+      })
+      .catch((error) => {
+        console.log(error, "An error occured");
+      });
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  React.useEffect(() => {
+    axios
+      .post("http://localhost:3000/admin/list", {
+        token,
+        skip: 0,
+        limit: 10000,
+        Type: "Admin",
+      })
+      .then((res) => {
+        console.log(res, "Response from PAI");
+        setAdmins(res.data.admins);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
+
+  return (
+    <Modal show={open} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>SubAdmins</Modal.Title>
+      </Modal.Header>
+      {admins !== [] &&
+        admins.map((admin) => (
+          <ModalBody
+            onClick={() => {
+              setSelectedsubadmin(admin._id);
+            }}
+            style={{
+              backgroundColor: selectedsubadmin === admin._id ? "red" : "white",
+            }}
+          >
+            {admin.Firstname + " " + admin.Lastname}
+          </ModalBody>
+        ))}
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Close
+        </Button>
+        <Button variant="primary" onClick={assigntosubadmin}>
+          Save Changes
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
--- src/components/Userlist/Userlist.js
+++ src/components/Userlist/Userlist.js
@@ -1,114 +1,410 @@
+/* eslint-disable no-undef */
+
 import * as React from "react";
-import { useState, useEffect } from "react";
-import { styled } from "@mui/material/styles";
+
+import { alpha } from "@mui/material/styles";
+import Box from "@mui/material/Box";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
-import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
 import TableRow from "@mui/material/TableRow";
+import TableSortLabel from "@mui/material/TableSortLabel";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 import Paper from "@mui/material/Paper";
+import Checkbox from "@mui/material/Checkbox";
+import { visuallyHidden } from "@mui/utils";
 import useToken from "../App/useToken";
-import { Routes, Route, useNavigate } from "react-router-dom";
-import { CheckBox } from "@mui/icons-material";
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
+import axios from "axios";
+import Subadminlist from "./Subadminlist";
+
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function getComparator(order, orderBy) {
+  return order === "desc"
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function stableSort(array, comparator) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) {
+      return order;
+    }
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
+
+const headCells = [
+  {
+    id: "Product",
+    numeric: false,
+    disablePadding: true,
+    label: "Product",
   },
-  [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
+  {
+    id: "Bank",
+    numeric: false,
+    disablePadding: true,
+    label: "Bank",
   },
-}));
-
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  "&:nth-of-type(odd)": {
-    backgroundColor: theme.palette.action.hover,
+  {
+    id: "Provider",
+    numeric: true,
+    disablePadding: false,
+    label: "Provider",
   },
-  flexDirection: "column",
-  // hide last border
-  "&:last-child td, &:last-child th": {
-    border: 0,
+
+  {
+    id: "Applied On:-",
+    numeric: true,
+    disablePadding: false,
+    label: "Applied On:-",
   },
-}));
+  {
+    id: "Status",
+    disablePadding: false,
+    label: "Status",
+  },
+];
 
-export default function Applicationlist() {
-  const navigate = useNavigate();
-  const { token } = useToken();
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:3000/application/list", {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify({
-        token,
-        resolveProduct: true,
-        resolveUser: true,
-        skip: 0,
-        limit: 10000,
-      }),
-    })
-      .then((data) => data.json())
-      .then((data) => {
-        setData(data.applications);
-
-        console.log(data, "Data for products");
-      })
-
-      .catch((error) => {
-        console.log(error, "error");
-      });
-  }, [token]);
+function EnhancedTableHead(props) {
+  const {
+    onSelectAllClick,
+    order,
+    orderBy,
+    numSelected,
+    rowCount,
+    onRequestSort,
+  } = props;
+  const createSortHandler = (property) => (event) => {
+    onRequestSort(event, property);
+  };
 
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 700 }} aria-label="customized table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell align="left">User</StyledTableCell>
-
-            <StyledTableCell align="left">Product</StyledTableCell>
-            <StyledTableCell align="center">Provider</StyledTableCell>
-            <StyledTableCell align="center">Applied On:-</StyledTableCell>
-            <StyledTableCell align="center">Status</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.map((item) => (
-            <StyledTableRow
-              key={item.Status}
-              onClick={() => navigate("/Applicationdetail", { state: item })}
+    <TableHead>
+      <TableRow>
+        <TableCell padding="checkbox">
+          <Checkbox
+            color="primary"
+            indeterminate={numSelected > 0 && numSelected < rowCount}
+            checked={rowCount > 0 && numSelected === rowCount}
+            onChange={onSelectAllClick}
+            inputProps={{
+              "aria-label": "select all desserts",
+            }}
+          />
+        </TableCell>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? "center" : "center"}
+            padding={headCell.disablePadding ? "none" : "normal"}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : "asc"}
+              onClick={createSortHandler(headCell.id)}
             >
-              <StyledTableCell component="th" scope="column" align="left">
-                <div>{`${item?.User?.Firstname ?? "Hitanshu"} ${
-                  item?.User?.Lastname ?? "Shah"
-                }`}</div>
-                <div>{`${item?.User?.Phone ?? "9137173437"}`}</div>
-              </StyledTableCell>
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <Box component="span" sx={visuallyHidden}>
+                  {order === "desc" ? "sorted descending" : "sorted ascending"}
+                </Box>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
 
-              <StyledTableCell align="left">
-                <img
-                  src={`http://localhost:3000/${item.Product.CoverImage}`}
-                  alt="BigCo Inc. logo"
-                  width={64}
-                  height={40}
-                />
-              </StyledTableCell>
-              <StyledTableCell align="center">
-                <div> {item.Product.BankName}</div>
-                <div> {item.Product.Name}</div>
-              </StyledTableCell>
-              <StyledTableCell align="center">
-                {item.createdAt.slice(0, 10)}
-              </StyledTableCell>
-              <StyledTableCell align="center">{item.Status}</StyledTableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+function EnhancedTableToolbar(props) {
+  const { numSelected, selected, setRefresh, refresh, handleOpen } = props;
+
+  const { token } = useToken();
+  const Statusupdate = async (status) => {
+    console.log("Press");
+
+    console.log("Entering EE");
+    // axios
+    //   .post("http://localhost:3000/user/update", {
+    //     token,
+    //     Status: status,
+    //     UserIds: selected,
+    //   })
+    //   .then((response) => {
+    //     console.log(response, "Response user update");
+    //     alert("Updated Succesfully!");
+    //     setRefresh(!refresh);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error, "An error occured!");
+    //   });
+  };
+
+  return (
+    <Toolbar
+      sx={{
+        pl: { sm: 2 },
+        pr: { xs: 1, sm: 1 },
+        ...(numSelected > 0 && {
+          bgcolor: (theme) =>
+            alpha(
+              theme.palette.primary.main,
+              theme.palette.action.activatedOpacity
+            ),
+        }),
+      }}
+    >
+      {numSelected > 0 ? (
+        <Typography
+          sx={{ flex: "1 1 100%" }}
+          color="inherit"
+          variant="subtitle1"
+          component="div"
+        >
+          {numSelected} selected
+        </Typography>
+      ) : (
+        <div
+          style={{
+            marginBottom: 10,
+            marginLeft: 2,
+            marginRight: 25,
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            marginTop: 20,
+            justifyContent: "space-between",
+          }}
+        >
+          <h5 style={{ color: "black", fontWeight: "500" }}>
+            Application List
+          </h5>
+        </div>
+      )}
+
+      {numSelected > 0 ? (
+        <>
+          <button
+            onClick={handleOpen}
+            style={{
+              backgroundColor: "white",
+              width: 90,
+              borderRadius: 20,
+              borderColor: "black",
+              color: "black",
+              marginRight: 20,
+              height: 40,
+            }}
+          >
+            Assign
+          </button>
+        </>
+      ) : null}
+    </Toolbar>
+  );
+}
+
+export default function Applicationlist() {
+  const { token } = useToken();
+
+  const [rows, setRows] = React.useState([]);
+  const [order, setOrder] = React.useState("asc");
+  const [orderBy, setOrderBy] = React.useState("Firstname");
+  const [selected, setSelected] = React.useState([]);
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(25);
+  const [refresh, setRefresh] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
+
+  React.useEffect(() => {
+    axios
+      .post("http://localhost:3000/application/list", {
+        token,
+        skip: 0,
+        limit: 10000,
+        resolveUser: true,
+        resolveProduct: true,
+      })
+      .then((res) => {
+        console.log(res, "Response from API");
+        setRows(res.data.applications);
+        // setRows(res.data.admins);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
+    setOrderBy(property);
+  };
+
+  const handleSelectAllClick = (event) => {
+    if (event.target.checked) {
+      const newSelected = rows.map((n) => n._id);
+      setSelected(newSelected);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleClick = (event, _id) => {
+    const selectedIndex = selected.indexOf(_id);
+    console.log(selectedIndex);
+    let newSelected = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, _id);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1)
+      );
+    }
+    setSelected(newSelected);
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const isSelected = (_id) => selected.indexOf(_id) !== -1;
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Paper sx={{ width: "100%", mb: 2 }}>
+        <EnhancedTableToolbar
+          numSelected={selected.length}
+          selected={selected}
+          refresh={refresh}
+          setRefresh={setRefresh}
+          handleOpen={handleOpen}
+        />
+        <TableContainer>
+          <Table sx={{ minWidth: 750 }} aria-labelledby="tableTitle">
+            <EnhancedTableHead
+              numSelected={selected.length}
+              order={order}
+              orderBy={orderBy}
+              onSelectAllClick={handleSelectAllClick}
+              onRequestSort={handleRequestSort}
+              rowCount={rows.length}
+            />
+            <TableBody>
+              {/* if you don't need to support IE11, you can replace the `stableSort` call with:
+             rows.sort(getComparator(order, orderBy)).slice() */}
+              {stableSort(rows, getComparator(order, orderBy))
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row, index) => {
+                  const isItemSelected = isSelected(row._id);
+                  const labelId = `enhanced-table-checkbox-${index}`;
+
+                  return (
+                    <TableRow
+                      hover
+                      onClick={(event) => handleClick(event, row._id)}
+                      role="checkbox"
+                      aria-checked={isItemSelected}
+                      tabIndex={-1}
+                      key={row.name}
+                      selected={isItemSelected}
+                    >
+                      <TableCell padding="checkbox">
+                        <Checkbox
+                          color="primary"
+                          checked={isItemSelected}
+                          inputProps={{
+                            "aria-labelledby": labelId,
+                          }}
+                        />
+                      </TableCell>
+                      <TableCell align="center">{row.Product.Name}</TableCell>
+                      <TableCell align="center">
+                        {row.Product.BankName}
+                      </TableCell>
+                      <TableCell
+                        component="th"
+                        id={labelId}
+                        scope="row"
+                        padding="none"
+                        align="center"
+                      >
+                        <img
+                          src={`http://localhost:3000/${row.Product.CoverImage}`}
+                          alt="BigCo Inc. logo"
+                          width={64}
+                          height={40}
+                        />
+                      </TableCell>
+
+                      <TableCell align="center">
+                        {row.createdAt.slice(0, 10)}
+                      </TableCell>
+                      <TableCell align="center">{row.Status}</TableCell>
+                    </TableRow>
+                  );
+                })}
+              {emptyRows > 0 && (
+                <TableRow
+                  style={{
+                    height: 53 * emptyRows,
+                  }}
+                >
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+        <Subadminlist open={open} setOpen={setOpen} Ids={selected} />
+      </Paper>
+    </Box>
   );
 }
